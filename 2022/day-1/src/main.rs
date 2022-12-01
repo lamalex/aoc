c957614b948e1f526b3f9559d71b619864ba6b70
Alex Launi <dev@launi.me>
2022-12-01 09:30:56 -0500

    AOC 2022

---
+++ 2022/day-1/src/main.rs
@@ -0,0 +1,14 @@
+use std::cmp::Reverse;
+
+fn main() {
+    let data = include_str!("input.txt");
+    let mut data: Vec<u64> = data
+        .split("\n\n")
+        .map(|e| e.split('\n').map(|e| e.parse::<u64>().unwrap()).sum())
+        .collect();
+
+    data.sort_unstable_by_key(|e| Reverse(*e));
+
+    println!("pt 1 {}", data.iter().nth(0).unwrap());
+    println!("pt 2 {}", data.iter().take(3).sum::<u64>());
+}
